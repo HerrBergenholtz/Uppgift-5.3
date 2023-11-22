97b0058203652e9ef0bef1f4a0dfa8482dce5497
HerrBergenholtz <zbdv8800@gmail.com>
2023-11-22 10:09:33 +0100

    Add project files.

---
+++ Uppgift 5.3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Uppgift_5._3
+{
+    class Program
+    {
+        public static void Main() 
+        {
+            try
+            {
+                int[] talArray = new int[3];
+                Console.WriteLine("Skriv in tre tal på olika rader");
+                for (int i = 0; i < talArray.Length; i++)
+                {
+                    talArray[i] = int.Parse(Console.ReadLine());
+                }
+                int summa = 0;
+                foreach (int i in talArray)
+                {
+                    summa += i;
+                }
+                Console.WriteLine($"Summan av alla tal är {summa}");
+                Console.ReadKey();
+
+            }
+            catch
+            {
+                Console.WriteLine("Du gav inte rätt input, försök igen");
+                Main();
+            }
+        }
+    }
+}
